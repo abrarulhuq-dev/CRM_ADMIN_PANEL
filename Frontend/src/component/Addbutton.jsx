45185222025d97686c7004a6b5f020551322e8de
abrar <abrar@gmail.com>
2025-08-29 02:25:32 +0530

    developed the addbutton with function of navigate

--- Frontend/src/component/Addbutton.jsx
+++ Frontend/src/component/Addbutton.jsx
@@ -1,18 +1,33 @@
+import { Plus } from 'lucide-react'
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-const Addbutton = () => {
+const Addbutton = ({name}) => {
 
-    const ispageapath = useLocation().pathname
+    const ispageapath = useLocation()
+    const navigate = useNavigate()
 
-    console.log(ispageapath)
+    const path = {
+        '/customers': 'Customers',
+        '/managers': 'Managers',
+        '/staffs' : 'Staffs',
+        '/departments' : 'Departments'
+    }
+
+    const btnpath = path[ispageapath.pathname]
+
+   
 
 
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className=' flex justify-end'>
+            <button onClick={()=> navigate(`/from/${name}`)} className='flex items-center gap-2  bg-primary py-4 px-3.5 text-white rounded-xl'>
+                Add {btnpath}
+                <Plus className='w-7 h-7'/>
+            </button>
+            
+        </div>
+    )
 }
 
 export default Addbutton
